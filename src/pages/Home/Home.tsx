4a1b790ab382003f8570767000c244b1957abcf0
Matt Hills <mph8344@rit.edu>
2023-05-02 14:21:30 -0400

    Fixed styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
-import "./Home.scss";
-import Grid from "../../components/Grid";
-import { useState } from "react";
-import Drawer from "../../components/Drawer";
-import Backdrop from "../../components/Backdrop";
+import './Home.scss';
+import { Grid, MusicGrid } from '../../components/Grid';
+import { useState } from 'react';
+import Drawer from '../../components/Drawer';
+import Backdrop from '../../components/Backdrop';
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,27 +13,22 @@ function Home() {
       <Drawer isOpen={isOpen}></Drawer>
       <Backdrop onClick={toggleDrawer} isOpen={isOpen}></Backdrop>
 
-      <div className="header">
-        <div onClick={toggleDrawer} className="dae-header">
+      <div className='header'>
+        <div onClick={toggleDrawer} className='dae-header'>
           DAE-ONE.XYZ
         </div>
       </div>
 
-      <div className="main-body">
-        <div className="content-left">
+      <div className='main-body'>
+        <div className='content-left'>
           <Grid withSound={false}></Grid>
+          {/* <MusicGrid></MusicGrid> */}
         </div>
-        <div className="divider">
-          <div className="silly-box"></div>
+        <div className='divider'>
+          <div className='silly-box'></div>
         </div>
-        <div className="content-right">
-          <div className="text">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          </div>
+        <div className='content-right'>
+          <div className='text'></div>
         </div>
       </div>
     </>
