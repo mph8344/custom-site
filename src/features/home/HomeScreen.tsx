ca802260dea72bb9f873895119daf86ca5aee1d3
Matt Hills <mph8344@rit.edu>
2025-06-13 17:32:08 -0400

    Some weird animation styling

--- src/features/home/HomeScreen.tsx
+++ src/features/home/HomeScreen.tsx
@@ -3,13 +3,19 @@ import styles from './HomeScreen.module.scss';
 export const HomeScreen = () => {
   return (
     <div className={styles.homeScreen}>
-      <div className={styles.pillars}>
+      <div className={styles.navigation}>
+        <div className={styles.divider}>
+          <span>{'main menu'}</span>
+        </div>
+        <div className={styles.backgroundPillar}></div>
+        <div className={styles.animatedPillar}></div>
+
+        {/* <div className={styles.pillars}>
         <div className={styles.navPillarOne}></div>
-        <div className={styles.divider}></div>
 
         <div className={styles.navPillarTwo}></div>
       </div>
-      <div className={styles.backgroundPillar}>
+      <div className={styles.backgroundPillar}> */}
         {/* <div className={styles.pillarBar}>
             <span>{'main menu'}</span>
           </div> */}
